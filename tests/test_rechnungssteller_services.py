73d57e76104573b4ea301e54aceb57ff0741a0b1
julian <julian.haase@uol.de>
2026-02-24 11:45:54 +0100

    rechnungssteller_services Tests ergänzt.

--- tests/test_rechnungssteller_services.py
+++ tests/test_rechnungssteller_services.py
@@ -4,7 +4,8 @@ import tempfile
 from unittest import TestCase
 
 from datenbank.connection import create_tabellen
-from services.rechnungssteller_services import neuen_rechnungsteller_erfassen, lade_alle_rechnungssteller_iban_dict, lade_alle_rechnungssteller_dict
+
+from services.rechnungssteller_services import *
 
 
 class Rechnungssteller_Test(TestCase):
@@ -39,7 +40,7 @@ class Rechnungssteller_Test(TestCase):
         self.assertFalse(ist_eingefuegt)
 
     def test_lade_alle_rechnungssteller_iban_dict(self):
-        self.assertEqual(None,lade_alle_rechnungssteller_iban_dict(self.db_path))
+        self.assertEqual(None, lade_alle_rechnungssteller_iban_dict(self.db_path))
         name = "Test Rechnungssteller"
         iban = "DE02300606010002474689"
         neuen_rechnungsteller_erfassen(name, iban, self.db_path)
@@ -47,19 +48,40 @@ class Rechnungssteller_Test(TestCase):
         rechnungsteller_dict = lade_alle_rechnungssteller_iban_dict(self.db_path)
         last_key, last_value = next(reversed(rechnungsteller_dict.items()))
         self.assertEqual(5, len(rechnungsteller_dict))
-        self.assertEqual(last_key,name)
-        self.assertEqual(last_value,iban)
-
-
+        self.assertEqual(last_key, name)
+        self.assertEqual(last_value, iban)
 
     def test_lade_alle_rechnungssteller_dict(self):
         self.assertEqual(None, lade_alle_rechnungssteller_dict(self.db_path))
+        name = "Test Rechnungssteller"
+        iban = "DE02300606010002474689"
+        neuen_rechnungsteller_erfassen(name, iban, self.db_path)
         self.testrechnungssteller_anlegen()
         rechnungsteller_dict = lade_alle_rechnungssteller_dict(self.db_path)
-        self.assertEqual(4, len(rechnungsteller_dict))
+        self.assertEqual(5, len(rechnungsteller_dict))
+        test_value = rechnungsteller_dict.get(1)  # erster Tabelleneintrag
+        self.assertEqual(test_value, name)
 
     def test_iban_aktualisieren(self):
-        self.fail()
+        name = "Test Rechnungssteller"
+        iban = ""
+        neuen_rechnungsteller_erfassen(name, iban, self.db_path)
+        test_iban = "DE02300606010002474689"
+        neuen_rechnungsteller_erfassen(name, iban, self.db_path)
+        self.assertNotEqual(test_iban, lade_iban(name, self.db_path))
+        iban_aktualisieren(name, test_iban, self.db_path)
+        self.assertEqual(test_iban, lade_iban(name, self.db_path))
 
     def test_lade_iban(self):
-        self.fail()
+        name = "Test Rechnungssteller"
+        iban = "DE02300606010002474689"
+        neuen_rechnungsteller_erfassen(name, iban, self.db_path)
+        self.assertEqual(iban, lade_iban(name, self.db_path))
+
+    def test_ist_valide_iban(self):
+        self.assertTrue(ist_valide_iban(""))
+        self.assertTrue(ist_valide_iban("DE02300606010002474689"))
+        # nur gültige IBAN werden akzeptiert
+        # https://ibanvalidieren.de/beispiele.html
+        self.assertFalse(ist_valide_iban("DE12345678901234567890"))
+        self.assertFalse(ist_valide_iban("00DE123456789012345678"))
